6ff989736dc3fa93c75f834c4a7056344902bebc
nilesh1903 <rudrawagh085@gmail.com>
2026-01-25 16:41:44 +0530

    update

--- my-app/src/services/orderService.js
+++ my-app/src/services/orderService.js
@@ -1,16 +1,18 @@
 import { db } from "../firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
-export const placeOrder = async (phone, table, items, total) => {
+export const placeOrder = async (customerName, phone, table, items, total) => {
   return await addDoc(
-    collection(db, "orders"),  // users/{phone} नको, direct orders ठेऊ
+    collection(db, "kitchenOrders"),
     {
+      customerName,
       phone,
       table,
       items,
       total,
-      status: "received",
-      paymentStatus: "pending",  // 👈 new field
+      status: "recived",       // kitchen workflow
+      paymentStatus: " paid", // payment pending
+      billPaid: false,          // default
       createdAt: serverTimestamp()
     }
   );
