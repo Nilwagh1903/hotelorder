1a7c0a3212dc823956e270fb3ca6d7ecc2badd61
nilesh1903 <rudrawagh085@gmail.com>
2026-01-25 17:17:40 +0530

    update

--- my-app/src/services/orderService.js
+++ my-app/src/services/orderService.js
@@ -2,17 +2,15 @@ import { db } from "../firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 export const placeOrder = async (customerName, phone, table, items, total) => {
-  return await addDoc(
-    collection(db, "kitchenOrders"),
-    {
-      customerName,
-      phone,
-      table,
-      items,
-      total,
-      status: "recived",       // kitchen workflow
-      billPaid: false,          // default
-      createdAt: serverTimestamp()
-    }
-  );
+  return await addDoc(collection(db, "kitchenOrders"), {
+    customerName,
+    phone,
+    table,
+    items,
+    total,
+    status: "new",
+    paymentStatus: "pending",
+    billPaid: false,
+    createdAt: serverTimestamp()
+  });
 };
